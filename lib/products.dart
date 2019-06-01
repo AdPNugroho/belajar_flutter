8f8fe4b10246d600fbd133bae32c618352c0aa4e
Adi Prasetyo Nugroho <portdevapn@gmail.com>
2019-06-01 14:44:11 +0800

    Optimasi List Loading Behaviour dengan ListView.Builder

--- lib/products.dart
+++ lib/products.dart
@@ -6,19 +6,24 @@ class Products extends StatelessWidget {
   // Products(this.products){
     print('[Product Widget] Constructor');
   }
+
+  Widget _buildProductItem(BuildContext context,int index){ //Custom method untuk Widget Rendering ListView.Builder dengan constructor context dan index
+    return Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/image.jpg',height: 50,width: 50,alignment: Alignment.center,),
+                Text(products[index]) //akses data List dengan mengakses index langsung dari Listnya
+              ],
+            ),
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Product Widget] Build');
-    return ListView( //Kolom Wrapper Vertical Row untuk List CardView
-        children: products //Perulangan data dari variabel products
-            .map((element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/image.jpg',height: 50,width: 50,alignment: Alignment.center,),
-                      Text(element)
-                    ],
-                  ),
-                ))
-            .toList());
+    return ListView.builder(  //Alternatif ListView widget untuk meningkatkan performa rendering data List
+      itemBuilder: _buildProductItem, //Method Widget
+      itemCount: products.length, //Hitung total data dari list Products
+    );
   }
 }
